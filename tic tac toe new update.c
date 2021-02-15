6352ac25df18c27d725469fe4c988a15671322e2
zis4215 <61169700+zis4215@users.noreply.github.com>
2021-02-15 20:23:56 +0700

    fix 3x3 vs player

--- tic tac toe new update.c
+++ tic tac toe new update.c
@@ -14,9 +14,23 @@ void help();
 void grid1();
 void grid2();
 void grid3();
+void resetGrid();
 int p, q, r, s,u,t;
+int skor1, skor2;
 char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+int StartTime() {
+    clock_t startInput;
+    startInput = clock();
 
+    return startInput;
+}
+
+int EndTime() {
+    clock_t endInput;
+    endInput = clock();
+
+    return endInput;
+}
 int checkwin();
 void board();
 
@@ -24,9 +38,9 @@ void board();
 int main()
 {
 	do {
-		printf("*========*\n[1] Mulai	[2] Keluar	[3] Cara Bermain	 [4] Tentang\nPilihan Anda : ");
+		printf("	======================================================================\n\n\n 			SELAMAT DATANG DI GAME TIC TAC TOE AE\n\n\n[1] Mulai	[2] Keluar	[3] Cara Bermain	 [4] Tentang\n\n\nPilihan Anda : ");
 		scanf("%i",&p);
-		
+
 		system("cls");
 		switch(p){
 			case 1 :
@@ -39,7 +53,7 @@ int main()
 			case 3 :
 				help();
 				break;
-			case 4 : 
+			case 4 :
 				about ();
 				break;
 			default:
@@ -51,9 +65,9 @@ int main()
 	}while(p!=2 || p<2);
 	return 0;
 }
-	
+
 void papan()
-{printf("*========*\nPilih Ukuran Papan\n[1] 3x3	[2] 5x5	[3] 7x7\nPilihan Anda : ");
+{printf("*#*#=================================================*#*#\n\n\nPilih Ukuran Papan\n\n[1] 3x3	[2] 5x5	[3] 7x7\n\nPilihan Anda : ");
 	scanf("%i",&q);
 	system("cls");
 	switch (q){
@@ -72,43 +86,48 @@ void papan()
 }
 
 void mode(){
-	printf("*========\n Pilih Mode Permainan[1] VS P	[2] VS AI\nPilihan Anda : ");
+	printf("*#*#=================================================*#*#\n\n\n Pilih Mode Permainan\n\n[1] VS P	[2] VS AI\n\nPilihan Anda : ");
 	scanf("%i",&r);
 	system("cls");
 	switch (r)
 	{
 		case 1 :
-			printf("*========*\nPilih Skor Maks\n[1] 3	[2] 5	[3] 7\nPilihan Anda : ");
+			printf("*#*#=================================================*#*#\n\n\nPilih Skor Maks\n\n[1] 3	[2] 5	[3] 7\n\nPilihan Anda : ");
 			scanf("%i",&t);
+			switch(t){
+                case 1: t = 3; break;
+                case 2: t = 5; break;
+                case 3: t = 7; break;
+			}
 			system("cls");
 			break;
-			
+
 		case 2 :
-			printf("*========*\nPilih Level Permainan[1] VS P	[2] VS AI\nPilihan Anda : ");
+			printf("*#*#=================================================*#*#\n\n\nPilih Level Permainan\n\n[1] VS P	[2] VS AI\n\nPilihan Anda : ");
 			scanf("%i",&t);
 			system("cls");
 			break;
 	}
 
 }
-/*void grid1 (){
-			printf("||===========||===========||===========||\n");
-			printf("||1          ||2          ||3          ||\n");
-			printf("||     -     ||     -     ||     -     ||\n");
-			printf("||           ||           ||           ||\n");
-			printf("||===========||===========||===========||\n");
-			printf("||4          ||5          ||6          ||\n");
-			printf("||     -     ||     -     ||     -     ||\n");
-			printf("||           ||           ||           ||\n");
-			printf("||===========||===========||===========||\n");
-			printf("||7          ||8          ||9          ||\n");
-			printf("||     -     ||     -     ||     -     ||\n");
-			printf("||           ||           ||           ||\n");
-			printf("||===========||===========||===========||\n\n\n");
-			scanf("%i",&s);
-	
-}
-}*/
+//void grid1 (){
+//			printf("||===========||===========||===========||\n");
+//			printf("||1          ||2          ||3          ||\n");
+//			printf("||     -     ||     -     ||     -     ||\n");
+//			printf("||           ||           ||           ||\n");
+//			printf("||===========||===========||===========||\n");
+//			printf("||4          ||5          ||6          ||\n");
+//			printf("||     -     ||     -     ||     -     ||\n");
+//			printf("||           ||           ||           ||\n");
+//			printf("||===========||===========||===========||\n");
+//			printf("||7          ||8          ||9          ||\n");
+//			printf("||     -     ||     -     ||     -     ||\n");
+//			printf("||           ||           ||           ||\n");
+//			printf("||===========||===========||===========||\n\n\n");
+//			scanf("%i",&s);
+//
+//}
+//}
 
 void grid2 (){
 			printf("=======================   PAPAN PERMAINAN   =======================\n\n");
@@ -134,10 +153,10 @@ void grid2 (){
 			printf("||===========||===========||===========||===========||===========||\n\n\n");\
 			scanf("%i",&t);
 
-	
+
 }
 void grid3 (){
-			printf("====================================   PAPAN PERMAINAN   ====================================\n\n");			
+			printf("====================================   PAPAN PERMAINAN   ====================================\n\n");
 			printf("||===========||===========||===========||===========||===========||===========||===========||\n");
 			printf("||1          ||2          ||3          ||4          ||5          ||6          ||7          ||\n");
 			printf("||     -     ||     -     ||     -     ||     -     ||     -     ||     -     ||     -     ||\n");
@@ -174,20 +193,20 @@ void help(){
 	do{
 	system("cls");
 	printf (" Mainkan aja \n\n\n");
-		printf("Masukan C untuk kembali : ");
+		printf("Masukan Y untuk kembali : ");
 		scanf("%c",&pilihan);
-	}while(tolower(pilihan) != 'c');
+	}while(tolower(pilihan) != 'y');
 }
 
 void about(){
 	char pilihan='N';
-	
+
 	do{
 		system("cls");
 		printf("||===========   About   ===========||\n");
-		printf("||  Game Sederhana                 ||\n");
-		printf("||      Tic Tac Toe                ||\n");
-		printf("||          Azis X Eben            ||\n");
+		printf("||                                 ||\n");
+		printf("||                                 ||\n");
+		printf("||                                 ||\n");
 		printf("||                                 ||\n");
 		printf("||=================================||\n\n\n");
 		printf("Masukan Y untuk kembali : ");
@@ -196,99 +215,125 @@ void about(){
 }
 
 void grid1 (){
-    int player = 1, i, choice;
+    int player = 1, i, choice, t;
+    double waktuInput;
+    skor1 = 0;
+    skor2 = 0;
 
     char mark;
-    do
-    {
-        board();
-        player = (player % 2) ? 1 : 2;
-
-        printf("Player %d, enter a number:  ", player);
-        scanf("%d", &choice);
-
-        mark = (player == 1) ? 'X' : 'O';
-
-        if (choice == 1 && square[1] == '1')
-            square[1] = mark;
-            
-        else if (choice == 2 && square[2] == '2')
-            square[2] = mark;
-            
-        else if (choice == 3 && square[3] == '3')
-            square[3] = mark;
-            
-        else if (choice == 4 && square[4] == '4')
-            square[4] = mark;
-            
-        else if (choice == 5 && square[5] == '5')
-            square[5] = mark;
-            
-        else if (choice == 6 && square[6] == '6')
-            square[6] = mark;
-            
-        else if (choice == 7 && square[7] == '7')
-            square[7] = mark;
-            
-        else if (choice == 8 && square[8] == '8')
-            square[8] = mark;
-            
-        else if (choice == 9 && square[9] == '9')
-            square[9] = mark;
-            
-        else
+    do{
+        do
         {
-            printf("Invalid move ");
+            board();
+            player = (player % 2) ? 1 : 2;
 
-            player--;
-            getch();
+            t=StartTime();
+            printf("Player %d, enter a number:  ", player);
+            scanf("%d", &choice);
+            t=EndTime()-t;
+            waktuInput=((double)t)/CLOCKS_PER_SEC;
+
+            mark = (player == 1) ? 'X' : 'O';
+
+            if(waktuInput<=10){
+                if (choice == 1 && square[1] == '1')
+                    square[1] = mark;
+
+                else if (choice == 2 && square[2] == '2')
+                    square[2] = mark;
+
+                else if (choice == 3 && square[3] == '3')
+                    square[3] = mark;
+
+                else if (choice == 4 && square[4] == '4')
+                    square[4] = mark;
+
+                else if (choice == 5 && square[5] == '5')
+                    square[5] = mark;
+
+                else if (choice == 6 && square[6] == '6')
+                    square[6] = mark;
+
+                else if (choice == 7 && square[7] == '7')
+                    square[7] = mark;
+
+                else if (choice == 8 && square[8] == '8')
+                    square[8] = mark;
+
+                else if (choice == 9 && square[9] == '9')
+                    square[9] = mark;
+
+                else
+                {
+                    printf("Invalid move ");
+
+                    player--;
+                    getch();
+                }
+            } else {
+                printf("Waktu Habis BEN !!!");
+                getch();
+            }
+
+
+            i = checkwin();
+
+            player++;
+        }while (i ==  - 1);
+
+        board();
+        player--;
+        if (i == 1){
+            printf("==>\aPlayer %d win ",player);
+            if(player==1){
+                 skor1++;
+            }
+
+            else{
+                skor2++;
+            }
         }
-        i = checkwin();
+        else{
+            printf("==>\aGame draw");
+        }
+        board();
+        resetGrid();
+        getch();
 
-        player++;
-    }while (i ==  - 1);
-    
-    board();
-    
-    if (i == 1)
-        printf("==>\aPlayer %d win ", --player);
-    else
-        printf("==>\aGame draw");
-
-    getch();
+    }while(skor1!=t && skor2!=t);
 
     return 0;
 }
 
-/*********************************************/
+/***************/
 int checkwin()
 {
     if (square[1] == square[2] && square[2] == square[3])
         return 1;
-        
+
     else if (square[4] == square[5] && square[5] == square[6])
         return 1;
-        
+
     else if (square[7] == square[8] && square[8] == square[9])
         return 1;
-        
+
     else if (square[1] == square[4] && square[4] == square[7])
         return 1;
-        
+
     else if (square[2] == square[5] && square[5] == square[8])
         return 1;
-        
+
     else if (square[3] == square[6] && square[6] == square[9])
         return 1;
-        
+
     else if (square[1] == square[5] && square[5] == square[9])
         return 1;
-        
+
     else if (square[3] == square[5] && square[5] == square[7])
         return 1;
-        
+
     else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
+        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7]
         != '7' && square[8] != '8' && square[9] != '9')
 
         return 0;
@@ -297,9 +342,9 @@ int checkwin()
 }
 
 
-/*******************************************************************
+/***********************
 FUNCTION TO DRAW BOARD OF TIC TAC TOE WITH PLAYERS MARK
- ********************************************************************/
+ ************************/
 
 void board()
 {
@@ -307,6 +352,7 @@ void board()
     printf("\n\n\tTic Tac Toe\n\n");
 
     printf("Player 1 (X)  -  Player 2 (O)\n\n\n");
+    printf("Skor Player 1 (X) : %d -  Skor Player 2 (O) : %d\n\n\n", skor1, skor2);
 
 
     printf("     |     |     \n");
@@ -325,21 +371,17 @@ void board()
     printf("     |     |     \n\n");
 }
 
-/*******************************************************************
+void resetGrid(){
+    char c='0';
+    int i;
+
+    for(i=0; i<10; i++){
+        square[i]=c;
+        c++;
+    }
+
+}
+
+/***********************
 END OF PROJECT
- ********************************************************************/
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ ************************/ 
